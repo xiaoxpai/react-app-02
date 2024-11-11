f0357646e8a901d2ff9527d4c5000816ab5f96f9
xiaoxpai Name <alf_work@outlook.com>
2024-11-11 15:31:06 +0800

    TODO: AllMeetups 从后端接口API获取数据，待开发

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,33 +1,12 @@
 import MeetupList from "../components/meetups/MeetupList";
 
 
-const people = [{
-    id: 0,
-    name: '凯瑟琳·约翰逊',
-    profession: '数学家',
-    address: '美国',
-}, {
-    id: 1,
-    name: '马里奥·莫利纳',
-    profession: '化学家',
-    address: '墨西哥',
-}, {
-    id: 2,
-    name: '穆罕默德·阿卜杜勒·萨拉姆',
-    profession: '物理学家',
-    address: '巴基斯坦',
-}, {
-    id: 3,
-    name: '珀西·莱温·朱利亚',
-    profession: '化学家',
-    address: '美国',
-}, {
-    id: 4,
-    name: '苏布拉马尼扬·钱德拉塞卡',
-    profession: '天体物理学家',
-    address: '印度',
-}];
-
+/**
+ * TODO 发送请求获取数据
+ *  1. 使用fetch发送请求，URL地址：http://10.151.110.52:8080/getMeets
+ * @returns {JSX.Element}
+ * @constructor
+ */
 function AllMeetupsPage() {
     return (
         <section>
@@ -36,13 +15,5 @@ function AllMeetupsPage() {
         </section>
     )
 }
-// <ul>
-//                 {people.map((meetup) => {
-//                     return (
-//                         <li key={meetup.id}>
-//                             {meetup.name} - {meetup.profession} - {meetup.address}
-//                         </li>
-//                     )
-//                 })}
-//             </ul>
+
 export default AllMeetupsPage;
