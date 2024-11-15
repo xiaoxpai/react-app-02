69045e9f8cf645d92b59ec26e501913edd6c513b
F1686352 <hzlh-mis-xiao.lh@foxconn.com>
2024-11-15 16:56:22 +0800

    FavoritesPage 页面实现逻辑 (#2)

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,26 @@
+import {useContext} from "react";
+import  FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 
 function FavoritesPage(){
+    const favoritesCtx = useContext(FavoritesContext);
+
+    //如果没有收藏的话，提示，你可以添加一些收藏
+    let content;
+    if (favoritesCtx.totalFavorites ===0){
+        content = <p>You got no favorites yet. Start adding some?</p>
+    }else {
+        //如果有收藏的话，显示收藏列表
+        content = <MeetupList meetups={favoritesCtx.favorites}/>
+    }
+
+
     return (
-        <div>Favorites Page</div>
+        <section>
+            <h1>My Favorites</h1>
+            {content}
+        </section>
     )
 }
 
