a2530d698e3a12c8c6669b6a4a7c8268c5c5e4f5
F1686352 <hzlh-mis-xiao.lh@foxconn.com>
2024-11-15 16:54:18 +0800

    在index.js 设置为全局管理状态 (#2)

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {FavoritesContextProvider} from "./store/favorites-context";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <FavoritesContextProvider>
     <React.StrictMode>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
     </React.StrictMode>
+    </FavoritesContextProvider>
 );
